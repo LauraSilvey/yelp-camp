9032de1e2198412e3fd664cd3fbfe34e062a3942
Laura Silvey <lsilvey@gmail.com>
2017-11-14 06:29:07 -0800

    added new view

--- app.js
+++ app.js
@@ -9,20 +9,33 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/campgrounds", function(req, res){
-  var campgrounds = [
+var campgrounds = [
     {name: "Morro Dunes RV Park", image: "http://images.goodsam.com/trailerlifedirectory/largefeatured/1000x/pho_100006986_06.jpg"},
     {name: "White Wolf Campground", image: "http://www.parkcamper.com/Yosemite-National-Park/white-wolf-campground-yosemite.jpg"},
     {name: "Manzanita Lake Campground", image: "http://www.parkcamper.com/LV/Manzanita-Lake-campground-Lassen-Volcanic-NP.jpg"},
   ]
 
+app.get("/campgrounds", function(req, res){
+  
+
   res.render("campgrounds", { campgrounds })
 });
 
 app.post("/campgrounds", function(req, res){
-  res.send("this is the post route")
   //get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {
+    name: name,
+    image: image,
+  }
+  campgrounds.push(newCampground);
   //redirect to campgrounds page
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
 });
 
 app.listen(3000);
