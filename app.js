ee940c3fe559e050806be15b44c462923cb7194b
Laura Silvey <lsilvey@gmail.com>
2017-11-15 16:16:10 -0800

    added database

--- app.js
+++ app.js
@@ -1,41 +1,91 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-  res.render("home");
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
 });
 
-var campgrounds = [
-    {name: "Morro Dunes RV Park", image: "http://images.goodsam.com/trailerlifedirectory/largefeatured/1000x/pho_100006986_06.jpg"},
-    {name: "White Wolf Campground", image: "http://www.parkcamper.com/Yosemite-National-Park/white-wolf-campground-yosemite.jpg"},
-    {name: "Manzanita Lake Campground", image: "http://www.parkcamper.com/LV/Manzanita-Lake-campground-Lassen-Volcanic-NP.jpg"},
-  ]
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "White Wolf Campground", 
+//     image: "http://www.parkcamper.com/Yosemite-National-Park/white-wolf-campground-yosemite.jpg",
+//     description: "Nestled above the Yosemite Valley.  Running water, but no showers.  Tent camping only."
+//   }, 
+//   function(err, campground){
+//        if(err){
+//         console.log(err);
+//        } else {
+//         console.log("newly created campground: ");
+//         console.log(campground);
+//        }
+// });
+
+// var campgrounds = [
+//     {name: "Morro Dunes RV Park", image: "http://images.goodsam.com/trailerlifedirectory/largefeatured/1000x/pho_100006986_06.jpg"},
+//     {name: "White Wolf Campground", image: "http://www.parkcamper.com/Yosemite-National-Park/white-wolf-campground-yosemite.jpg"},
+//     {name: "Manzanita Lake Campground", image: "http://www.parkcamper.com/LV/Manzanita-Lake-campground-Lassen-Volcanic-NP.jpg"},
+//   ]
+
+
+app.get("/", function(req, res){
+  res.render("home");
+});  
 
 app.get("/campgrounds", function(req, res){
-  
-
-  res.render("campgrounds", { campgrounds })
+  //Get all campgrounds from database
+  Campground.find({}, function(err, campgrounds){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("index", { campgrounds });
+    }
+  });  
 });
 
 app.post("/campgrounds", function(req, res){
   //get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
+  var description = req.body.description;
   var newCampground = {
     name: name,
     image: image,
+    description: description,
   }
-  campgrounds.push(newCampground);
-  //redirect to campgrounds page
-  res.redirect("/campgrounds");
+  //Create a new campground and save to database
+  Campground.create(newCampground, function(err, newEntry){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res){
-  res.render("new.ejs");
+  res.render("new");
+});
+
+//Show
+app.get("/campgrounds/:id", function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("show", {campground: foundCampground});
+    }
+  });
 });
 
 app.listen(3000);
