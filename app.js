6f35a23ab194cad98fbc613b3dba3a58f704257e
Laura Silvey <lsilvey@gmail.com>
2017-12-05 13:15:56 -0800

    setup new routes for comments

--- app.js
+++ app.js
@@ -77,4 +77,28 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
   });  
 });
 
+app.post("/campgrounds/:id/comments", function(req, res){
+  //lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground){
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function(err, comment){
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      })
+
+    }
+  })
+  //create new comment
+  //connect new comment to campground
+  //redirect to show campground show page
+})
+
 app.listen(3000);
