656e4a1d0263963c72bb7e7cebd699654b32f1a9
Laura Silvey <lsilvey@gmail.com>
2018-01-04 09:16:35 -0800

    change port

--- app.js
+++ app.js
@@ -54,8 +54,6 @@ app.use(function(req, res, next){
  app.use("/campgrounds", campgroundRoutes);
 
 
-
-
-
-
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+ console.log('app running at port 3000...');
+});
